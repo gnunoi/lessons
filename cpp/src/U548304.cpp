63c9cb46f2959d82fc9623469707fb931262acd6
gnunoi <gnunoi@163.com>
2025-03-28 17:25:50 +0800

    Update

---
+++ cpp/src/U548304.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+bool isDivisibleBy11(const string& num) 
+{
+    int oddSum = 0, evenSum = 0;
+    for (int i = 0; i < num.length(); ++i) {
+        int digit = num[i] - '0';
+        if (i % 2 == 0)  oddSum += digit;
+        else evenSum += digit;
+    }
+    return (oddSum - evenSum) % 11 == 0;
+}
+
+int main() 
+{
+    string num;
+    cin >> num;
+    if (isDivisibleBy11(num)) cout << "Yes" << endl;
+    else cout << "No" << endl;
+    
+    return 0;
+}
--- cpp/src/error/B2007-E2.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-// 计算两数之和
-int main()
-{
-  int a, b;
-  cin >> a >> b;
-  cout << a - b;
-}
